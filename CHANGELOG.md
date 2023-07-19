b9a441309dd58d711951f5f60574c7f95acfa08b
Guilherme Dantas <guidanoli@proton.me>
2023-07-19 11:16:58 -0300

    feat: add authority factory

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,19 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- `AuthorityFactory`: Allows anyone to deploy `Authority` contracts. Supports deterministic deployment.
+
+### Removed
+
+- `docker-bake*.hcl`
+- `.dockerignore`
+- `onchain/Dockerfile`
+- `offchain` (now maintained in [`cartesi/rollups-node`](https://github.com/cartesi/rollups-node))
+- `grpc-interfaces` submodule
+- `rollups-cli` package
+
 ## [1.0.0] 2023-08-22
 
 ### Added
---
+++ onchain/rollups/contracts/consensus/authority/IAuthorityFactory.sol
@@ -0,0 +1,46 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.8;
+
+import {Authority} from "./Authority.sol";
+
+/// @title Authority Factory interface
+interface IAuthorityFactory {
+    // Events
+
+    /// @notice A new authority was deployed.
+    /// @param authorityOwner The initial authority owner
+    /// @param authority The authority
+    /// @dev MUST be triggered on a successful call to `newAuthority`.
+    event AuthorityCreated(address authorityOwner, Authority authority);
+
+    // Permissionless functions
+
+    /// @notice Deploy a new authority.
+    /// @param _authorityOwner The initial authority owner
+    /// @return The authority
+    /// @dev On success, MUST emit an `AuthorityCreated` event.
+    function newAuthority(address _authorityOwner) external returns (Authority);
+
+    /// @notice Deploy a new authority deterministically.
+    /// @param _authorityOwner The initial authority owner
+    /// @param _salt The salt used to deterministically generate the authority address
+    /// @return The authority
+    /// @dev On success, MUST emit an `AuthorityCreated` event.
+    function newAuthority(
+        address _authorityOwner,
+        bytes32 _salt
+    ) external returns (Authority);
+
+    /// @notice Calculate the address of an authority to be deployed deterministically.
+    /// @param _authorityOwner The initial authority owner
+    /// @param _salt The salt used to deterministically generate the authority address
+    /// @return The deterministic authority address
+    /// @dev Beware that only the `newAuthority` function with the `_salt` parameter
+    ///      is able to deterministically deploy an authority.
+    function calculateAuthorityAddress(
+        address _authorityOwner,
+        bytes32 _salt
+    ) external view returns (address);
+}
