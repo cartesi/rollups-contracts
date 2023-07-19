7c2baf5dffa897f9c94fe9d49baa33cbac4d3651
Guilherme Dantas <guidanoli@proton.me>
2023-07-19 11:16:58 -0300

    feat: add authority factory

---
+++ onchain/rollups/test/foundry/consensus/authority/AuthorityFactory.t.sol
@@ -0,0 +1,97 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+/// @title Authority Factory Test
+pragma solidity ^0.8.8;
+
+import {Test} from "forge-std/Test.sol";
+import {AuthorityFactory} from "contracts/consensus/authority/AuthorityFactory.sol";
+import {Authority} from "contracts/consensus/authority/Authority.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+contract AuthorityFactoryTest is Test {
+    AuthorityFactory factory;
+
+    // event emitted in the factory
+    event AuthorityCreated(address authorityOwner, Authority authority);
+
+    function setUp() public {
+        factory = new AuthorityFactory();
+    }
+
+    function testNewAuthority(address _authorityOwner) public {
+        vm.assume(_authorityOwner != address(0));
+
+        vm.recordLogs();
+
+        Authority authority = factory.newAuthority(_authorityOwner);
+
+        testNewAuthorityAux(_authorityOwner, authority);
+    }
+
+    function testNewAuthorityAux(
+        address _authorityOwner,
+        Authority _authority
+    ) internal {
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        uint256 numOfAuthorityCreated;
+
+        for (uint256 i; i < entries.length; ++i) {
+            Vm.Log memory entry = entries[i];
+
+            if (
+                entry.emitter == address(factory) &&
+                entry.topics[0] == AuthorityCreated.selector
+            ) {
+                ++numOfAuthorityCreated;
+
+                address a;
+                address b;
+
+                (a, b) = abi.decode(entry.data, (address, address));
+
+                assertEq(_authorityOwner, a);
+                assertEq(address(_authority), b);
+            }
+        }
+
+        assertEq(numOfAuthorityCreated, 1);
+
+        // call to check authority's owner
+        assertEq(_authority.owner(), _authorityOwner);
+    }
+
+    function testNewAuthorityDeterministic(
+        address _authorityOwner,
+        bytes32 _salt
+    ) public {
+        vm.assume(_authorityOwner != address(0));
+
+        address precalculatedAddress = factory.calculateAuthorityAddress(
+            _authorityOwner,
+            _salt
+        );
+
+        vm.recordLogs();
+
+        Authority authority = factory.newAuthority(_authorityOwner, _salt);
+
+        testNewAuthorityAux(_authorityOwner, authority);
+
+        // Precalculated address must match actual address
+        assertEq(precalculatedAddress, address(authority));
+
+        precalculatedAddress = factory.calculateAuthorityAddress(
+            _authorityOwner,
+            _salt
+        );
+
+        // Precalculated address must STILL match actual address
+        assertEq(precalculatedAddress, address(authority));
+
+        // Cannot deploy an authority with the same salt twice
+        vm.expectRevert();
+        factory.newAuthority(_authorityOwner, _salt);
+    }
+}
