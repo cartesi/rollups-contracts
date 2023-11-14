5a0d4045a593d9306edfe4db8f4c68a39cd42cd9
Guilherme Dantas <guidanoli@proton.me>
2023-11-14 15:57:43 -0300

    feat!: revert on failed ERC-20 deposit

--- onchain/rollups/contracts/portals/ERC20Portal.sol
+++ onchain/rollups/contracts/portals/ERC20Portal.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.8;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import {IERC20Portal} from "./IERC20Portal.sol";
 import {InputRelay} from "../inputs/InputRelay.sol";
@@ -15,6 +16,8 @@ import {InputEncoding} from "../common/InputEncoding.sol";
 /// @notice This contract allows anyone to perform transfers of
 /// ERC-20 tokens to a DApp while informing the off-chain machine.
 contract ERC20Portal is InputRelay, IERC20Portal {
+    using SafeERC20 for IERC20;
+
     /// @notice Constructs the portal.
     /// @param _inputBox The input box used by the portal
     constructor(IInputBox _inputBox) InputRelay(_inputBox) {}
@@ -25,10 +28,10 @@ contract ERC20Portal is InputRelay, IERC20Portal {
         uint256 _amount,
         bytes calldata _execLayerData
     ) external override {
-        bool success = _token.transferFrom(msg.sender, _dapp, _amount);
+        _token.safeTransferFrom(msg.sender, _dapp, _amount);
 
         bytes memory input = InputEncoding.encodeERC20Deposit(
-            success,
+            true,
             _token,
             msg.sender,
             _amount,
