c4db89e689adf20e5f297eb8e6de47733d65edd7
Guilherme Dantas <guidanoli@proton.me>
2023-11-24 11:00:11 -0300

    test: add `LibBytes.sol`

---
+++ onchain/rollups/test/foundry/util/LibBytes.sol
@@ -0,0 +1,20 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.8;
+
+library LibBytes {
+    /// @notice Generate an address from a byte array.
+    /// @param data The byte array
+    /// @return The address
+    function hashToAddress(bytes memory data) internal pure returns (address) {
+        return address(bytes20(keccak256(data)));
+    }
+
+    /// @notice Generate a 256-bit unsigned integer from a byte array.
+    /// @param data The byte array
+    /// @return The 256-bit unsigned integer
+    function hashToUint256(bytes memory data) internal pure returns (uint256) {
+        return uint256(keccak256(data));
+    }
+}
