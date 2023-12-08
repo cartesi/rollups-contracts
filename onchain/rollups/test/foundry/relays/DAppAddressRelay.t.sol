3abc7433b94d3d732636f0fc6e2da511b03fbb98
Guilherme Dantas <guidanoli@proton.me>
2023-12-08 09:27:52 -0300

    feat: create base contract for testing ERC-165

--- onchain/rollups/test/foundry/relays/DAppAddressRelay.t.sol
+++ onchain/rollups/test/foundry/relays/DAppAddressRelay.t.sol
@@ -1,68 +1,71 @@
 // (c) Cartesi and individual authors (see AUTHORS)
 // SPDX-License-Identifier: Apache-2.0 (see LICENSE)
 
-/// @title DApp Address Relay Test
 pragma solidity ^0.8.8;
 
-import {Test} from "forge-std/Test.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import {IDAppAddressRelay} from "contracts/relays/IDAppAddressRelay.sol";
+
 import {DAppAddressRelay} from "contracts/relays/DAppAddressRelay.sol";
+import {IDAppAddressRelay} from "contracts/relays/IDAppAddressRelay.sol";
 import {IInputBox} from "contracts/inputs/IInputBox.sol";
+import {IInputRelay} from "contracts/inputs/IInputRelay.sol";
 import {InputBox} from "contracts/inputs/InputBox.sol";
 import {InputEncoding} from "contracts/common/InputEncoding.sol";
-import {IInputRelay} from "contracts/inputs/IInputRelay.sol";
 
-contract DAppAddressRelayTest is Test {
-    IInputBox inputBox;
-    IDAppAddressRelay relay;
+import {ERC165Test} from "../util/ERC165Test.sol";
 
-    event InputAdded(
-        address indexed dapp,
-        uint256 indexed inputIndex,
-        address sender,
-        bytes input
-    );
+contract DAppAddressRelayTest is ERC165Test {
+    address _alice;
+    IInputBox _inputBox;
+    IDAppAddressRelay _relay;
 
     function setUp() public {
-        inputBox = new InputBox();
-        relay = new DAppAddressRelay(inputBox);
+        _alice = vm.addr(1);
+        _inputBox = IInputBox(vm.addr(2));
+        _relay = new DAppAddressRelay(_inputBox);
     }
 
-    function testSupportsInterface(bytes4 _randomInterfaceId) public {
-        assertTrue(
-            relay.supportsInterface(type(IDAppAddressRelay).interfaceId)
-        );
-        assertTrue(relay.supportsInterface(type(IInputRelay).interfaceId));
-        assertTrue(relay.supportsInterface(type(IERC165).interfaceId));
+    function getERC165Contract() public view override returns (IERC165) {
+        return _relay;
+    }
 
-        assertFalse(relay.supportsInterface(bytes4(0xffffffff)));
-
-        vm.assume(_randomInterfaceId != type(IDAppAddressRelay).interfaceId);
-        vm.assume(_randomInterfaceId != type(IInputRelay).interfaceId);
-        vm.assume(_randomInterfaceId != type(IERC165).interfaceId);
-        assertFalse(relay.supportsInterface(_randomInterfaceId));
+    function getSupportedInterfaces()
+        public
+        pure
+        override
+        returns (bytes4[] memory)
+    {
+        bytes4[] memory interfaceIds = new bytes4[](2);
+        interfaceIds[0] = type(IDAppAddressRelay).interfaceId;
+        interfaceIds[1] = type(IInputRelay).interfaceId;
+        return interfaceIds;
     }
 
     function testGetInputBox() public {
-        assertEq(address(relay.getInputBox()), address(inputBox));
+        assertEq(address(_relay.getInputBox()), address(_inputBox));
     }
 
-    function testRelayDAppAddress(address _dapp) public {
-        // Check the DApp's input box before
-        assertEq(inputBox.getNumberOfInputs(_dapp), 0);
+    function testRelayDAppAddress(address dapp) public {
+        bytes memory input = _encodeInput(dapp);
 
-        // Construct the DApp address relay input
-        bytes memory input = abi.encodePacked(_dapp);
+        bytes memory addInput = _encodeAddInput(dapp, input);
 
-        // Expect InputAdded to be emitted with the right arguments
-        vm.expectEmit(true, true, false, true, address(inputBox));
-        emit InputAdded(_dapp, 0, address(relay), input);
+        vm.mockCall(address(_inputBox), addInput, abi.encode(bytes32(0)));
 
-        // Relay the DApp's address
-        relay.relayDAppAddress(_dapp);
+        vm.expectCall(address(_inputBox), addInput, 1);
 
-        // Check the DApp's input box after
-        assertEq(inputBox.getNumberOfInputs(_dapp), 1);
+        vm.prank(_alice);
+        _relay.relayDAppAddress(dapp);
+    }
+
+    function _encodeInput(address dapp) internal pure returns (bytes memory) {
+        return InputEncoding.encodeDAppAddressRelay(dapp);
+    }
+
+    function _encodeAddInput(
+        address dapp,
+        bytes memory input
+    ) internal pure returns (bytes memory) {
+        return abi.encodeCall(IInputBox.addInput, (dapp, input));
     }
 }
