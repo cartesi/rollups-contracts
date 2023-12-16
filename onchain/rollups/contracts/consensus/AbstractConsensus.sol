7d56318274e05ce7ed47ae17669a01a5e7b13b0e
Guilherme Dantas <guidanoli@proton.me>
2023-12-16 17:54:29 -0300

    fix: remove empty set check

--- onchain/rollups/contracts/consensus/AbstractConsensus.sol
+++ onchain/rollups/contracts/consensus/AbstractConsensus.sol
@@ -5,14 +5,11 @@ pragma solidity ^0.8.8;
 
 import {IConsensus} from "./IConsensus.sol";
 import {InputRange} from "../common/InputRange.sol";
-import {LibInputRange} from "../library/LibInputRange.sol";
 
 /// @notice Stores epoch hashes for several DApps and input ranges.
 /// @dev This contract was designed to be inherited by implementations of the `IConsensus` interface
 /// that only need a simple mechanism of storage and retrieval of epoch hashes.
 abstract contract AbstractConsensus is IConsensus {
-    using LibInputRange for InputRange;
-
     /// @notice Indexes epoch hashes by DApp address, first input index and last input index.
     mapping(address => mapping(uint256 => mapping(uint256 => bytes32)))
         private _epochHashes;
@@ -34,16 +31,12 @@ abstract contract AbstractConsensus is IConsensus {
     /// @param dapp The DApp contract address
     /// @param r The input range
     /// @param epochHash The epoch hash
-    /// @dev Raises an `InputRangeIsEmptySet` error if `r` represents the empty set.
     /// @dev On successs, emits a `ClaimAcceptance` event.
     function _acceptClaim(
         address dapp,
         InputRange calldata r,
         bytes32 epochHash
     ) internal {
-        if (r.isEmptySet()) {
-            revert InputRangeIsEmptySet(dapp, r, epochHash);
-        }
         _epochHashes[dapp][r.firstInputIndex][r.lastInputIndex] = epochHash;
         emit ClaimAcceptance(dapp, r, epochHash);
     }
