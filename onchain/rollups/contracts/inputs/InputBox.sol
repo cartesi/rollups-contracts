a88a2e3a17ce69ccba0f26e4667778ba5893c708
Guilherme Dantas <guidanoli@proton.me>
2023-12-21 11:37:44 -0300

    feat: document the `IInputBox` interface

--- onchain/rollups/contracts/inputs/InputBox.sol
+++ onchain/rollups/contracts/inputs/InputBox.sol
@@ -5,62 +5,53 @@ pragma solidity ^0.8.8;
 
 import {IInputBox} from "./IInputBox.sol";
 import {LibInput} from "../library/LibInput.sol";
+import {CanonicalMachine} from "../common/CanonicalMachine.sol";
 
-/// @title Input Box
-///
-/// @notice Trustless and permissionless contract that receives arbitrary blobs
-/// (called "inputs") from anyone and adds a compound hash to an append-only list
-/// (called "input box"). Each application has its own input box.
-///
-/// The hash that is stored on-chain is composed by the hash of the input blob,
-/// the block number and timestamp, the input sender address, and the input index.
-///
-/// Data availability is guaranteed by the emission of `InputAdded` events
-/// on every successful call to `addInput`. This ensures that inputs can be
-/// retrieved by anyone at any time, without having to rely on centralized data
-/// providers.
-///
-/// From the perspective of this contract, inputs are encoding-agnostic byte
-/// arrays. It is up to the application to interpret, validate and act upon inputs.
 contract InputBox is IInputBox {
-    /// @notice Mapping from application address to list of input hashes.
-    /// @dev See the `getNumberOfInputs`, `getInputHash` and `addInput` functions.
-    mapping(address => bytes32[]) internal inputBoxes;
+    /// @notice Arrays of input hashes, indexed by application address.
+    mapping(address => bytes32[]) private _inputBoxes;
 
+    /// @inheritdoc IInputBox
     function addInput(
-        address _app,
-        bytes calldata _input
+        address app,
+        bytes calldata payload
     ) external override returns (bytes32) {
-        bytes32[] storage inputBox = inputBoxes[_app];
-        uint256 inputIndex = inputBox.length;
+        require(
+            payload.length <= CanonicalMachine.INPUT_MAX_SIZE,
+            "InputBox: payload too large"
+        );
+
+        bytes32[] storage inputBox = _inputBoxes[app];
+
+        uint256 index = inputBox.length;
 
         bytes32 inputHash = LibInput.computeInputHash(
             msg.sender,
             block.number,
             block.timestamp,
-            _input,
-            inputIndex
+            index,
+            payload
         );
 
-        // add input to the input box
         inputBox.push(inputHash);
 
-        // block.number and timestamp can be retrieved by the event metadata itself
-        emit InputAdded(_app, inputIndex, msg.sender, _input);
+        emit InputAdded(app, index, msg.sender, payload);
 
         return inputHash;
     }
 
+    /// @inheritdoc IInputBox
     function getNumberOfInputs(
-        address _app
+        address app
     ) external view override returns (uint256) {
-        return inputBoxes[_app].length;
+        return _inputBoxes[app].length;
     }
 
+    /// @inheritdoc IInputBox
     function getInputHash(
-        address _app,
-        uint256 _index
+        address app,
+        uint256 index
     ) external view override returns (bytes32) {
-        return inputBoxes[_app][_index];
+        return _inputBoxes[app][index];
     }
 }
