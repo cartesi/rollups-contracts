5559379f172eb8a1a5da39195e0cd33e7a6a3b10
Zehui Zheng <work996mail@gmail.com>
2024-04-24 18:36:12 +0800

    feat: safe erc20 delegatecall

---
+++ contracts/delegatecall/SafeERC20Transfer.sol
@@ -0,0 +1,15 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.20;
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SafeERC20Transfer {
+    using SafeERC20 for IERC20;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) external {
+        token.safeTransfer(to, value);
+    }
+}
