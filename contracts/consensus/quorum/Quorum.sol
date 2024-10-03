d425fe17a8cac532cb8e18c85423e23175a3f9a4
Guilherme Dantas <guidanoli@proton.me>
2024-10-03 13:34:48 -0300

    feat: create `IQuorum`

--- contracts/consensus/quorum/Quorum.sol
+++ contracts/consensus/quorum/Quorum.sol
@@ -5,15 +5,10 @@ pragma solidity ^0.8.8;
 
 import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 
+import {IQuorum} from "./IQuorum.sol";
 import {AbstractConsensus} from "../AbstractConsensus.sol";
 
-/// @notice A consensus model controlled by a small, immutable set of `n` validators.
-/// @notice You can know the value of `n` by calling the `numOfValidators` function.
-/// @notice Upon construction, each validator is assigned a unique number between 1 and `n`.
-/// These numbers are used internally instead of addresses for gas optimization reasons.
-/// @notice You can list the validators in the quorum by calling the `validatorById`
-/// function for each ID from 1 to `n`.
-contract Quorum is AbstractConsensus {
+contract Quorum is IQuorum, AbstractConsensus {
     using BitMaps for BitMaps.BitMap;
 
     /// @notice The total number of validators.
@@ -66,18 +61,11 @@ contract Quorum is AbstractConsensus {
         _numOfValidators = n;
     }
 
-    /// @notice Submit a claim.
-    /// @param appContract The application contract address
-    /// @param lastProcessedBlockNumber The number of the last processed block
-    /// @param claim The output Merkle root hash
-    /// @dev Fires a `ClaimSubmission` event if the message sender is a validator.
-    /// @dev Fires a `ClaimAcceptance` event if the claim reaches a majority.
-    /// @dev Can only be called by a validator.
     function submitClaim(
         address appContract,
         uint256 lastProcessedBlockNumber,
         bytes32 claim
-    ) external {
+    ) external override {
         uint256 id = _validatorId[msg.sender];
         require(id > 0, "Quorum: caller is not validator");
 
@@ -102,55 +90,38 @@ contract Quorum is AbstractConsensus {
         }
     }
 
-    /// @notice Get the number of validators.
-    function numOfValidators() external view returns (uint256) {
+    function numOfValidators() external view override returns (uint256) {
         return _numOfValidators;
     }
 
-    /// @notice Get the ID of a validator.
-    /// @param validator The validator address
-    /// @dev Validators have IDs greater than zero.
-    /// @dev Non-validators are assigned to ID zero.
-    function validatorId(address validator) external view returns (uint256) {
+    function validatorId(
+        address validator
+    ) external view override returns (uint256) {
         return _validatorId[validator];
     }
 
-    /// @notice Get the address of a validator by its ID.
-    /// @param id The validator ID
-    /// @dev Validator IDs range from 1 to `N`, the total number of validators.
-    /// @dev Invalid IDs map to address zero.
-    function validatorById(uint256 id) external view returns (address) {
+    function validatorById(
+        uint256 id
+    ) external view override returns (address) {
         return _validatorById[id];
     }
 
-    /// @notice Get the number of validators in favor of a claim.
-    /// @param appContract The application contract address
-    /// @param lastProcessedBlockNumber The number of the last processed block
-    /// @param claim The output Merkle root hash
-    /// @return Number of validators in favor of claim
     function numOfValidatorsInFavorOf(
         address appContract,
         uint256 lastProcessedBlockNumber,
         bytes32 claim
-    ) external view returns (uint256) {
+    ) external view override returns (uint256) {
         return
             _getVotes(appContract, lastProcessedBlockNumber, claim)
                 .inFavorCount;
     }
 
-    /// @notice Check whether a validator is in favor of a claim.
-    /// @param appContract The application contract address
-    /// @param lastProcessedBlockNumber The number of the last processed block
-    /// @param claim The output Merkle root hash
-    /// @param id The ID of the validator
-    /// @return Whether validator is in favor of claim
-    /// @dev Assumes the provided ID is valid.
     function isValidatorInFavorOf(
         address appContract,
         uint256 lastProcessedBlockNumber,
         bytes32 claim,
         uint256 id
-    ) external view returns (bool) {
+    ) external view override returns (bool) {
         return
             _getVotes(appContract, lastProcessedBlockNumber, claim)
                 .inFavorById
