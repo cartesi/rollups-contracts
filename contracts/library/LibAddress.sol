a935a74d78254558c89efba22720a0680ab12119
Guilherme Dantas <guidanoli@proton.me>
2024-11-02 18:25:31 -0300

    refactor: move `InsufficientFunds` to interface

--- contracts/library/LibAddress.sol
+++ contracts/library/LibAddress.sol
@@ -8,29 +8,23 @@ import {LibError} from "../library/LibError.sol";
 library LibAddress {
     using LibError for bytes;
 
-    /// @notice Caller does not have enough Ether to make call
-    /// @param caller The address of the contract making the call
-    /// @param value The value being transmitted through the call
-    /// @param balance The current contract balance
-    error InsufficientFunds(address caller, uint256 value, uint256 balance);
-
     /// @notice Perform a low level call and raise error if failed
     /// @param destination The address that will be called
     /// @param value The amount of Wei to be transferred through the call
     /// @param payload The payload, which—in the case of Solidity
     /// contracts—encodes a function call
-    /// @dev Raises an `InsufficientFunds` error
-    /// if the caller has less than `value` Wei
+    /// @return Whether the caller had enough Ether to make the call,
+    /// and the balance before the call
     function safeCall(
         address destination,
         uint256 value,
         bytes memory payload
-    ) internal {
+    ) internal returns (bool, uint256) {
         address caller = address(this);
         uint256 balance = caller.balance;
 
         if (value > balance) {
-            revert InsufficientFunds(caller, value, balance);
+            return (false, balance);
         }
 
         bool success;
@@ -41,6 +35,8 @@ library LibAddress {
         if (!success) {
             returndata.raise();
         }
+
+        return (true, balance);
     }
 
     /// @notice Perform a delegate call and raise error if failed
