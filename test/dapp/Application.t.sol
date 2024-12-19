8292d60e3f1569fa32661a025ac5f2bb256623b4
Guilherme Dantas <guidanoli@proton.me>
2024-12-19 12:55:05 -0300

    refactor: remove DataAvailability

--- test/dapp/Application.t.sol
+++ test/dapp/Application.t.sol
@@ -16,7 +16,6 @@ import {IOwnable} from "contracts/access/IOwnable.sol";
 import {LibAddress} from "contracts/library/LibAddress.sol";
 import {InputBox} from "contracts/inputs/InputBox.sol";
 import {IInputBox} from "contracts/inputs/IInputBox.sol";
-import {DataAvailability} from "contracts/common/DataAvailability.sol";
 
 import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
@@ -348,10 +347,7 @@ contract ApplicationTest is TestBase, OwnableTest {
         );
         _inputBox = new InputBox();
         _consensus = new Authority(_authorityOwner, _epochLength);
-        _dataAvailability = abi.encodeCall(
-            DataAvailability.InputBox,
-            (_inputBox)
-        );
+        _dataAvailability = new bytes(0);
         _appContract = new Application(
             _consensus,
             _appOwner,
