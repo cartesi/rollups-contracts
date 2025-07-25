4a8512070bf2289a1f9db41da984462f987b1779
Guilherme Dantas <guidanoli@proton.me>
2025-07-25 16:16:08 -0300

    Add Metadata lib

---
+++ src/common/Metadata.sol
@@ -0,0 +1,8 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.8;
+
+library Metadata {
+    uint256 constant MAJOR_VERSION = 3;
+}
