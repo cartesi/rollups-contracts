25e5ac1f80d5987c12c2a57e873e58ba19c52d37
Guilherme Dantas <guidanoli@proton.me>
2025-07-26 08:05:31 -0300

    Add getDataAvailabilityInterfaceIds

--- src/app/interfaces/IAppMetadata.sol
+++ src/app/interfaces/IAppMetadata.sol
@@ -10,4 +10,8 @@ interface IAppMetadata {
 
     /// @notice Get the major version of Cartesi Rollups Contracts used by the application.
     function getCartesiRollupsContractsMajorVersion() external pure returns (uint256);
+
+    /// @notice Get the ID of the data availability interface used by the application.
+    /// @dev This can be the XOR of more than one interface ID.
+    function getDataAvailabilityInterfaceId() external pure returns (bytes4);
 }
