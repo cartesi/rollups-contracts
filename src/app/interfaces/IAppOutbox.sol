e64b08e148c15857fa1c245c901d542e15551d77
Guilherme Dantas <guidanoli@proton.me>
2025-07-27 10:12:40 -0300

    Improve `IAppOutbox` docs

--- src/app/interfaces/IAppOutbox.sol
+++ src/app/interfaces/IAppOutbox.sol
@@ -25,46 +25,50 @@ interface IAppOutbox {
     /// @param balance The current application contract balance
     error InsufficientFunds(uint256 value, uint256 balance);
 
-    /// @notice Raised when the output hashes siblings array has an invalid size.
-    /// @dev Please consult `CanonicalMachine` for the maximum number of outputs.
+    /// @notice Could not validate an output, because the output hashes siblings array has an invalid length.
+    /// @dev Please consult `CanonicalMachine` for the maximum number of outputs,
+    /// which corresponds to the expected length of the output hashes siblings array.
     error InvalidOutputHashesSiblingsArrayLength();
 
-    /// @notice Raised when the computed outputs Merkle root is invalid.
-    error InvalidOutputsMerkleRoot(bytes32 outputsMerkleRoot);
+    /// @notice Could not validate an output, because the computed outputs Merkle root is invalid.
+    /// @param computedOutputsMerkleRoot The computed outputs Merkle root
+    error InvalidOutputsMerkleRoot(bytes32 computedOutputsMerkleRoot);
 
-    /// @notice Get number of outputs that have been executed.
+    /// @notice Get the number of outputs that have been executed.
+    /// @dev Outputs can be executed in any order, so do not assume that
+    /// all executed outputs have indices smaller than this value.
     function getNumberOfExecutedOutputs() external view returns (uint256);
 
     /// @notice Check whether an output has been executed.
-    /// @param outputIndex The index of output
+    /// @param outputIndex The output index
     /// @return Whether the output has been executed before
     function wasOutputExecuted(uint256 outputIndex) external view returns (bool);
 
     /// @notice Validate an output hash.
     /// @param outputHash The output hash
-    /// @param proof The proof used to validate the output against
-    ///              a claim accepted to the current outputs Merkle root validator contract
-    /// @dev May raise `InvalidOutputHashesSiblingsArrayLength`
-    /// or `InvalidOutputsMerkleRoot`.
+    /// @param proof The proof used to validate the output
+    /// @dev MAY raise an `InvalidOutputHashesSiblingsArrayLength` error.
+    /// @dev MAY raise an `InvalidOutputsMerkleRoot` error.
     function validateOutputHash(bytes32 outputHash, OutputValidityProof calldata proof)
         external
         view;
 
     /// @notice Validate an output.
     /// @param output The output
-    /// @param proof The proof used to validate the output against
-    ///              a claim accepted to the current outputs Merkle root validator contract
-    /// @dev May raise any of the errors raised by `validateOutputHash`.
+    /// @param proof The proof used to validate the output
+    /// @dev MAY raise any of the errors raised by `validateOutputHash`.
     function validateOutput(bytes calldata output, OutputValidityProof calldata proof)
         external
         view;
 
     /// @notice Execute an output.
     /// @param output The output
-    /// @param proof The proof used to validate the output against
-    ///              a claim accepted to the current outputs Merkle root validator contract
+    /// @param proof The proof used to validate the output
     /// @dev On a successful execution, emits a `OutputExecuted` event.
-    /// @dev May raise any of the errors raised by `validateOutput`,
+    /// @dev MAY raise any of the errors raised by `validateOutput`.
+    /// @dev MAY raise an `OutputNotExecutable` error.
+    /// @dev MAY raise an `OutputNotReexecutable` error.
+    /// @dev May raise any of the errors raised by `validateOutput`.
     /// as well as `OutputNotExecutable` and `OutputNotReexecutable`.
     function executeOutput(bytes calldata output, OutputValidityProof calldata proof)
         external;
