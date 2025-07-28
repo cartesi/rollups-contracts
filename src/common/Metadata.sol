d5725e4752d34ecd616b29862a4186dd1ad8808d
Guilherme Dantas <guidanoli@proton.me>
2025-07-28 08:09:59 -0300

    Remove Metadata lib

--- src/common/Metadata.sol
+++
@@ -1,8 +0,0 @@
-// (c) Cartesi and individual authors (see AUTHORS)
-// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
-
-pragma solidity ^0.8.8;
-
-library Metadata {
-    uint256 constant MAJOR_VERSION = 3;
-}
