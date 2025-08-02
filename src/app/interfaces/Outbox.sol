fc1918904730d9681cefbf0c9871d868dce857be
Guilherme Dantas <guidanoli@proton.me>
2025-08-02 18:08:19 -0300

    Remove Outbox -> EventEmitter inheritance

--- src/app/interfaces/Outbox.sol
+++ src/app/interfaces/Outbox.sol
@@ -3,11 +3,10 @@
 
 pragma solidity ^0.8.8;
 
-import {EventEmitter} from "./EventEmitter.sol";
 import {OutputValidityProof} from "../../common/OutputValidityProof.sol";
 
 /// @notice Allows the validation and execution of outputs.
-interface Outbox is EventEmitter {
+interface Outbox {
     /// @notice MUST trigger when an output is executed.
     /// @param outputIndex The index of the output
     /// @param output The output
