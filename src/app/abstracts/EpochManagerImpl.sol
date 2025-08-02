f82165d6be9149684b5a21a28f08fdd1292db7d5
Guilherme Dantas <guidanoli@proton.me>
2025-08-02 18:07:54 -0300

    Remove EpochManagerImpl

--- src/app/abstracts/EpochManagerImpl.sol
+++
@@ -1,8 +0,0 @@
-// (c) Cartesi and individual authors (see AUTHORS)
-// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
-
-pragma solidity ^0.8.27;
-
-import {EpochManager} from "../interfaces/EpochManager.sol";
-
-abstract contract EpochManagerImpl is EpochManager {}
