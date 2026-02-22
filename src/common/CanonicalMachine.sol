d99f893d855a92bc343a5c97d40a2de21bea4960
Guilherme Dantas <guidanoli@proton.me>
2026-02-22 12:13:06 -0300

    Add `LibWithdrawalConfig` library and tests

--- src/common/CanonicalMachine.sol
+++ src/common/CanonicalMachine.sol
@@ -11,6 +11,12 @@ library CanonicalMachine {
     /// @notice Maximum input size (64 kilobytes).
     uint64 constant INPUT_MAX_SIZE = 1 << 16;
 
+    /// @notice Log2 of memory size.
+    uint8 constant LOG2_MEMORY_SIZE = 64;
+
     /// @notice Log2 of maximum number of outputs.
     uint8 constant LOG2_MAX_OUTPUTS = 63;
+
+    /// @notice Log2 of data block size.
+    uint8 constant LOG2_DATA_BLOCK_SIZE = 5;
 }
---
+++ src/library/LibWithdrawalConfig.sol
@@ -0,0 +1,40 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.8;
+
+import {CanonicalMachine} from "../common/CanonicalMachine.sol";
+import {WithdrawalConfig} from "../common/WithdrawalConfig.sol";
+
+library LibWithdrawalConfig {
+    function isValid(WithdrawalConfig memory withdrawalConfig)
+        internal
+        pure
+        returns (bool)
+    {
+        // The addition below cannot overflow because `3 * type(uint8).max <= type(uint256).max`.
+        uint256 log2AccountsDriveSize = uint256(CanonicalMachine.LOG2_DATA_BLOCK_SIZE)
+            + uint256(withdrawalConfig.log2MaxNumOfAccounts)
+            + uint256(withdrawalConfig.log2LeavesPerAccount);
+
+        // The addition below cannot overflow because `type(uint8).max + 1 <= type(uint256).max`.
+        uint256 accountsDriveEndIndex =
+            uint256(withdrawalConfig.accountsDriveStartIndex) + 1;
+
+        // The left-shift below can overflow, so we need to check for overflow afterwards.
+        uint256 accountsDriveEnd = accountsDriveEndIndex << log2AccountsDriveSize;
+        if ((accountsDriveEnd >> log2AccountsDriveSize) != accountsDriveEndIndex) {
+            return false;
+        }
+
+        // forge-lint: disable-next-line(incorrect-shift)
+        uint256 memorySize = 1 << CanonicalMachine.LOG2_MEMORY_SIZE;
+
+        // Check if the accounts drive would end past the machine memory boundaries.
+        if (accountsDriveEnd > memorySize) {
+            return false;
+        }
+
+        return true;
+    }
+}
