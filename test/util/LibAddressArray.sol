01f065e7edce5e9e9f756a5e2a4b2c9848e6fe94
Guilherme Dantas <guidanoli@proton.me>
2026-02-28 09:30:53 -0300

    Add functions to `LibAddressArray` and tests
    - Remove `addrs` function

--- test/util/LibAddressArray.sol
+++ test/util/LibAddressArray.sol
@@ -6,6 +6,26 @@ pragma solidity ^0.8.22;
 import {Vm} from "forge-std-1.9.6/src/Vm.sol";
 
 library LibAddressArray {
+    function randomAddressIn(Vm vm, address[] memory array)
+        internal
+        returns (address addr)
+    {
+        require(array.length > 0, "Cannot sample random element from empty array");
+        addr = array[vm.randomUint(0, array.length - 1)];
+    }
+
+    function randomAddressNotIn(Vm vm, address[] memory array)
+        internal
+        returns (address addr)
+    {
+        while (true) {
+            addr = vm.randomAddress();
+            if (!contains(array, addr)) {
+                break;
+            }
+        }
+    }
+
     function contains(address[] memory array, address elem) internal pure returns (bool) {
         for (uint256 i; i < array.length; ++i) {
             if (array[i] == elem) {
@@ -13,12 +33,5 @@ library LibAddressArray {
             }
         }
         return false;
-    }
-
-    function addrs(Vm vm, uint256 n) internal pure returns (address[] memory array) {
-        array = new address[](n);
-        for (uint256 i; i < n; ++i) {
-            array[i] = vm.addr(i + 1);
-        }
     }
 }
