d56d78b698e14c8268e02154d76e2911fb93e1f2
Guilherme Dantas <guidanoli@proton.me>
2026-03-21 19:00:18 -0300

    Add `RollupsContract` abstract contract

---
+++ src/common/RollupsContract.sol
@@ -0,0 +1,27 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.26;
+
+import {IVersionGetter} from "./IVersionGetter.sol";
+
+abstract contract RollupsContract is IVersionGetter {
+    function version()
+        external
+        pure
+        override
+        returns (
+            uint64 major,
+            uint64 minor,
+            uint64 patch,
+            string memory preRelease,
+            string memory buildMetadata
+        )
+    {
+        major = 3;
+        minor = 0;
+        patch = 0;
+        preRelease = "alpha.3";
+        buildMetadata = "";
+    }
+}
