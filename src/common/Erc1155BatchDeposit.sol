8d566f5d711b7ccc8708a547d1f6ad5095271608
Guilherme Dantas <guidanoli@proton.me>
2026-07-02 23:15:49 -0300

    Fix Erc1155BatchDeposit NatSpec

--- src/common/Erc1155BatchDeposit.sol
+++ src/common/Erc1155BatchDeposit.sol
@@ -5,11 +5,11 @@ pragma solidity ^0.8.30;
 
 import {IERC1155} from "@openzeppelin-contracts-5.2.0/token/ERC1155/IERC1155.sol";
 
-/// @notice An ERC-1155 single token deposit
+/// @notice An ERC-1155 batch token deposit
 /// @param token The token contract
 /// @param sender The token sender
 /// @param tokenIds The token identifiers
-/// @param value The token amounts per token type
+/// @param values The token amounts per token type
 struct Erc1155BatchDeposit {
     IERC1155 token;
     address sender;
